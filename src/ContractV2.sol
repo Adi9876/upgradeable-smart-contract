73e6b2eb846319bcab5ca23da405484809bcf3ea
Adi9876 <begin1625@gmail.com>
2025-06-22 21:06:58 +0530

    ContractV2 with similar as V1

--- src/ContractV2.sol
+++ src/ContractV2.sol
@@ -1,8 +1,24 @@
 // SPDX-License-Identifier: LICENSE MIT
 pragma solidity 0.8.20;
 
-contract ContractV2 {
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract ContractV2 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     uint256 internal number;
+
+
+    ////////////////////////////// 
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init();
+    }
+    //////////////////////////////
 
     function getNumber() public view returns (uint256) {
         return number;
@@ -15,4 +31,9 @@ contract ContractV2 {
     function setNumber(uint256 _number) external {
         // number = _number;
     }
+
+
+    ////////////////////////
+    function _authorizeUpgrade(address newImplementation) internal override {}
+    ////////////////////////
 }
